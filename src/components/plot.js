daf962d0414027c7c1048f41a9fc803e4273c36c
Corina Adam <adam97corina@gmail.com>
2020-03-11 16:23:31 +0200

    plot function

--- src/components/plot.js
+++ src/components/plot.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import * as THREE from "three";
-const OrbitControls = require('three-orbit-controls')(THREE);
-
+const OrbitControls = require("three-orbit-controls")(THREE);
 
 const WIDTH = window.innerWidth,
   HEIGHT = window.innerHeight,
-  VIEW_ANGLE = 90,
+  VIEW_ANGLE = 45,
   ASPECT = WIDTH / HEIGHT,
-  NEAR = 50,
-  FAR = 0;
+  NEAR = 0.1,
+  FAR = 100,
+  segments = 20;
 
 const FunctionPlotter = ({ pso }) => {
   let [scene] = useState(new THREE.Scene());
@@ -23,6 +23,7 @@ const FunctionPlotter = ({ pso }) => {
       window.addEventListener("resize", onWindowResize, false);
       init();
       animate();
+      createGraph();
       initParticles(pso.particles);
     }
   }, [pso]);
@@ -33,11 +34,11 @@ const FunctionPlotter = ({ pso }) => {
         if (pso.iterationNum < 200) {
           pso.iterate();
           for (let i = 0; i < pso.particles.length; i++) {
-              if(canvasParticles[i]){
-                  canvasParticles[i].position.x = pso.particles[i].position[0];
-                  canvasParticles[i].position.y = pso.particles[i].position[1];
-                  canvasParticles[i].position.z = pso.particles[i].fitness;
-              }
+            if (canvasParticles[i]) {
+              canvasParticles[i].position.x = pso.particles[i].position[0];
+              canvasParticles[i].position.y = pso.particles[i].position[1];
+              canvasParticles[i].position.z = pso.particles[i].fitness;
+            }
           }
         } else {
           clearInterval(intervalId);
@@ -54,17 +55,17 @@ const FunctionPlotter = ({ pso }) => {
     renderer.setClearColor(0xdddddd, 1);
     renderer.clear();
 
-    camera.position.set(1, 1, 60);
+    camera.position.set(3, -3, 60);
     scene.add(camera);
 
     const light = new THREE.PointLight(0xffffff);
     light.position.set(0, 100, 100);
     scene.add(light);
 
-    const axesHelper = new THREE.AxesHelper( 100 );
-    scene.add( axesHelper );
+    const axesHelper = new THREE.AxesHelper(100);
+    scene.add(axesHelper);
 
-    new OrbitControls( camera, renderer.domElement );
+    new OrbitControls(camera, renderer.domElement);
   };
 
   const animate = () => {
@@ -79,7 +80,10 @@ const FunctionPlotter = ({ pso }) => {
   const initParticles = particles => {
     const particlesToAdd = [];
     particles.forEach(particle => {
-      const particleToAdd = createParticle(...particle.position, particle.fitness);
+      const particleToAdd = createParticle(
+        ...particle.position,
+        particle.fitness
+      );
       particlesToAdd.push(particleToAdd);
       scene.add(particleToAdd);
     });
@@ -94,6 +98,72 @@ const FunctionPlotter = ({ pso }) => {
     mesh.position.set(x, y, z);
     return mesh;
   };
+
+  function createGraph() {
+    const xMin = pso.fitnessFunction.dimensions[0].min;
+    const xMax = pso.fitnessFunction.dimensions[0].max;
+    const yMin = pso.fitnessFunction.dimensions[1].min;
+    const yMax = pso.fitnessFunction.dimensions[1].max;
+
+    const xRange = xMax - xMin;
+    const yRange = yMax - yMin;
+    const zFunc = pso.fitnessFunction.compute;
+    const meshFunction = function(x, y, target) {
+      x = xRange * x + xMin;
+      y = yRange * y + yMin;
+      var z = zFunc(x, y);
+      if (isNaN(z)) target.set(0, 0, 0 );
+      else target.set(x, y, z);
+    };
+
+    const graphGeometry = new THREE.ParametricGeometry(
+      meshFunction,
+      segments,
+      segments,
+      true
+    );
+
+    graphGeometry.computeBoundingBox();
+    const zMin = graphGeometry.boundingBox.min.z;
+    const zMax = graphGeometry.boundingBox.max.z;
+    const zRange = zMax - zMin;
+
+    let color, point, face, numberOfSides, vertexIndex;
+    let faceIndices = ["a", "b", "c", "d"];
+
+    for (let i = 0; i < graphGeometry.vertices.length; i++) {
+      point = graphGeometry.vertices[i];
+      color = new THREE.Color(0x0000ff);
+      color.setHSL((0.7 * (zMax - point.z)) / zRange, 1, 0.5);
+      graphGeometry.colors[i] = color;
+    }
+
+    for (let i = 0; i < graphGeometry.faces.length; i++) {
+      face = graphGeometry.faces[i];
+      numberOfSides = face instanceof THREE.Face3 ? 3 : 4;
+      for (let j = 0; j < numberOfSides; j++) {
+        vertexIndex = face[faceIndices[j]];
+        face.vertexColors[j] = graphGeometry.colors[vertexIndex];
+      }
+    }
+
+    const wireTexture = new THREE.ImageUtils.loadTexture("images/square.png");
+    wireTexture.wrapS = wireTexture.wrapT = THREE.RepeatWrapping;
+    wireTexture.repeat.set(40, 40);
+
+    const wireMaterial = new THREE.MeshBasicMaterial({
+      map: wireTexture,
+      vertexColors: THREE.VertexColors,
+      side: THREE.DoubleSide,
+      transparent: true,
+      opacity: 0.3
+    });
+    wireMaterial.map.repeat.set(segments, segments);
+
+    const graphMesh = new THREE.Mesh(graphGeometry, wireMaterial);
+    graphMesh.doubleSided = true;
+    scene.add(graphMesh);
+  }
 
   const onWindowResize = () => {
     camera.aspect = window.innerWidth / window.innerHeight;
