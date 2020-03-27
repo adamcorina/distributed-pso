1f9075cbeae5affcc1fddca6e7ad72ff787b3e9b
Corina Adam <adam97corina@gmail.com>
2020-03-27 19:13:57 +0200

    make controls draggable

--- src/components/controls/controls.js
+++ src/components/controls/controls.js
@@ -1,75 +1,90 @@
-import React, { useState } from "react";
+import React from "react";
 import Dropdown from "react-dropdown";
+import Draggable from 'react-draggable';
 import "react-dropdown/style.css";
 
 import { ALGORITHMS, FUNCTIONS } from "../../lib/utils/constants";
 
 import "./controls.css";
 
-const Controls = ({ pause, start, changeAlgorithm, changeFunction, playState, algorithmTag, functionTag }) => {
+const Controls = ({
+  pause,
+  start,
+  changeAlgorithm,
+  changeFunction,
+  playState,
+  algorithmTag,
+  functionTag
+}) => {
   return (
-    <table className="controls">
-      <tr>
-        <td colspan="3">
-          <div className="subtitle">All nodes:</div>
-        </td>
-      </tr>
-      <tr>
-        <td colspan="3">
-          <div className="btn" onClick={start}>
-            Restart
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td colspan="1">
-          <div className="description">Algorithm:</div>
-        </td>
-        <td colspan="2">
-          <Dropdown options={Object.values(ALGORITHMS)} value={algorithmTag} onChange={
-            (e) => {
-              changeAlgorithm(e.value);
-            }
-          }/>
-        </td>
-      </tr>
-      <tr>
-        <td colspan="1">
-          <div className="description">Function:</div>
-        </td>
-        <td colspan="2">
-          <Dropdown options={Object.values(FUNCTIONS)} value={functionTag} onChange={
-            (e) => {
-              changeFunction(e.value);
-            }
-          }/>
-        </td>
-      </tr>
-      <tr>
-        <td colspan="3">
-          <hr />
-        </td>
-      </tr>
-      <tr>
-        <td colspan="3">
-          <td>
-            <div className="subtitle">Local node:</div>
+    <Draggable>
+      <table className="controls">
+        <tr>
+          <td colspan="3">
+            <div className="subtitle">All nodes:</div>
           </td>
-        </td>
-      </tr>
-      <tr>
-        <td colspan="3">
-          <div
-            className="btn"
-            onClick={() => {
-              pause();
-            }}
-          >
-            {playState}
-          </div>
-        </td>
-      </tr>
-    </table>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <div className="btn" onClick={start}>
+              Restart
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="1">
+            <div className="description">Algorithm:</div>
+          </td>
+          <td colspan="2">
+            <Dropdown
+              options={Object.values(ALGORITHMS)}
+              value={algorithmTag}
+              onChange={e => {
+                changeAlgorithm(e.value);
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td colspan="1">
+            <div className="description">Function:</div>
+          </td>
+          <td colspan="2">
+            <Dropdown
+              options={Object.values(FUNCTIONS)}
+              value={functionTag}
+              onChange={e => {
+                changeFunction(e.value);
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <hr />
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <td>
+              <div className="subtitle">Local node:</div>
+            </td>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">
+            <div
+              className="btn"
+              onClick={() => {
+                pause();
+              }}
+            >
+              {playState}
+            </div>
+          </td>
+        </tr>
+      </table>
+    </Draggable>
   );
 };
 
